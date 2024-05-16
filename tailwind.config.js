22af4db4772021e166a314bced4d19928c3ad889
Daniel <github@djaekel.com>
2024-05-16 11:51:35 +0200

    feat: add search player layout + footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,7 @@ module.exports = {
             },
 
             text_shadow: {
+                xs: '0 1px 1px var(--tw-shadow-color)',
                 sm: '0 1px 2px var(--tw-shadow-color)',
                 DEFAULT: '0 2px 4px var(--tw-shadow-color)',
                 lg: '0 8px 16px var(--tw-shadow-color)',
